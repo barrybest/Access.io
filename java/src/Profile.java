9e9813423d1fa1ee876947d223473ffacf777360
Cameron Shannon <cpshanno@usc.edu>
2020-04-07 23:40:43 -0400

    Cleaning up Repository
    cleaning up repository

---
+++ java/src/Profile.java
@@ -0,0 +1,5 @@
+public class Profile {
+	public static void main(String [] args) {
+		System.out.println("Declan's test.");
+	}
+}
--- src/Profile.java
+++
@@ -1,5 +0,0 @@
-public class Profile {
-	public static void main(String [] args) {
-		System.out.println("Declan's test.");
-	}
-}
