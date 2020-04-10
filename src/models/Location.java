06932a17b5b565d746b275a75de6c3c437f9d6e6
Catherine Turner <cvturner@usc.edu>
2020-04-10 14:59:14 -0700

    Added starter code to Location.java

--- src/models/Location.java
+++ src/models/Location.java
@@ -1,7 +1,42 @@
 package models;
 
+import java.util.ArrayList;
+
 public class Location {
-	public static void main(String[] args) {
-		System.out.println("This is where I (Declan) will work on the location class.");
+	public int locationID;
+	public ArrayList<Integer> reviews;
+	
+	public Location(int locID) {
+		this.locationID = locID;
+	}
+	
+	public String getName(int locationID) {
+		String name = "";
+		// Use MySQL here
+		return name;
+	}
+	
+	public String getAddress(int locationID) {
+		String address = "";
+		// Use MySQL here
+		return address;
+	}
+	
+	public ArrayList<Integer> getAverages(int locationID) {
+		ArrayList<Integer> averages = new ArrayList<Integer>();
+		// Use MySQL here
+		return averages;
+	}
+	
+	public int getPhone(int locationID) {
+		int phoneNum = 0;
+		// Use MySQL here
+		return phoneNum;
+	}
+	
+	public String getURL(int locationID) {
+		String URL = "";
+		// Use MySQL here
+		return URL;
 	}
 }
