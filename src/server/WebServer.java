ae9575afd360a392d6dd4ff6b7f0c19f060ebe08
Declan <bbest@usc.edu>
2020-04-10 15:29:03 -0600

    i dont really know what i am doing tbh

--- src/server/WebServer.java
+++ src/server/WebServer.java
@@ -1,7 +1,17 @@
 package server;
 
 public class WebServer {
+	
+	//web server needs to deal with HTTP requests from the front end
+	
+	//accept requests and get the information requested from the front end
+	
+	//call models to retrieve the data, will need to be sent back to the client (JSON??)
+	
+	public WebServer(int port) {
+		
+	}
 	public static void main (String[] args) {
-		System.out.println("What is good!");
+		new WebServer(11111);
 	}
 }
