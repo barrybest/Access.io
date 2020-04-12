a3d2ccc86773559d676dd5156010734ac66aa321
Cameron Shannon <cpshanno@usc.edu>
2020-04-12 19:58:38 -0400

    changes to profile

--- sql/DBDeply.sql
+++ sql/DBDeply.sql
@@ -2,26 +2,40 @@ drop database Accessio;
 create database Accessio;
 use Accessio;
 
--- this is good --
+-- user table
 create table Users(
 	UserID INT PRIMARY KEY NOT NULL auto_increment,
     Email VARCHAR(100) NOT NULL,
     Name VARCHAR(100) NOT NULL,
     Username VARCHAR(100) NOT NULL,
-    Password VARCHAR(20) NOT NULL
+    Password VARCHAR(20) NOT NULL,
+    City VARCHAR(1000),
+    Stars DOUBLE,
+    Verified BOOLEAN,
+    Handicap VARCHAR(1000)
 );
--- this should be fine --
+
+-- profile picture table
+create table ProfilePictures(
+	PictureID INT primary key not null auto_increment,
+	ReviewID INT not null,
+	ImageData VARBINARY(MAX) not null
+);
+
+-- location table
 create table Locations(
 	LocationID INT PRIMARY KEY NOT NULL auto_increment,
     LocationName VARCHAR(1000) NOT NULL,
     Address VARCHAR(1000) NOT NULL,
     PhoneNumber VARCHAR(200) not null,
     Website VARCHAR(200) not null,
-    ElevatorRating double, -- these values can be empty on default, will be filled as users review --
+    -- these values can be empty on default, will be filled as users review
+    ElevatorRating double, 
     RampRating double,
     DoorRating double,
     Other double
 );
+
 -- this is good, may take some kinks to work out --
 create table Ratings(
 	RatingID INT primary key auto_increment not null,
@@ -34,21 +48,23 @@ create table Ratings(
     foreign key fk1(UserID) references Users(UserID),
     foreign key fk2(LocationID) references Locations(LocationID)
 );
--- this looks good --
+
+-- review table
 create table Reviews(
-	ReviewID int primary key not null auto_increment,
+	ReviewID INT primary key not null auto_increment,
     LocationID INT not null,
-    UserID int not null,
+    UserID INT not null,
     Review VARCHAR(2000) not null,
+    Upvotes INT not null,
+    Downvotes INT not null,
     foreign key fk1(LocationID) references Locations(LocationID),
     foreign key fk2(UserID) references Users(UserID)
 );
--- there is no primary key for this table --
-create table ReviewResponse(
-	ReviewID int not null,
-    Upvotes int not null,
-    Downvotes int not null,
-    foreign key fk1(ReviewID) references Reviews(ReviewID)
-);
 
+-- review pictures table
+create table Review Pictures(
+	PictureID INT primary key not null auto_increment,
+	ReviewID INT not null,
+	ImageData VARBINARY(MAX) not null
+);
             
--- src/models/Profile.java
+++ src/models/Profile.java
@@ -1,9 +1,30 @@
 package models;
+
+import java.io.FileInputStream;
+import java.util.ArrayList;
+
 public class Profile {
-	public static void main(String [] args) {
-		System.out.println("Declan's test.");
-		System.out.println("Austin's test.");
-		System.out.println("active branch?");
-		System.out.println("Setting up Cam's pull request.");
+	String name;
+	String city;
+	ArrayList<Review> reviews;
+	double stars;
+	boolean verified;
+	String handicap;
+	FileInputStream imageStream;
+	
+	public Profile(String name,
+				   String city,
+				   ArrayList<Review> reviews,
+				   double stars,
+				   boolean verified,
+				   String handicap,
+				   FileInputStream imageStream) {
+		this.name = name;
+		this.city = city;
+		this.reviews = reviews;
+		this.stars = stars;
+		this.verified = verified;
+		this.handicap = handicap;
+		this.imageStream = imageStream;
 	}
 }
