b6972f5efb02c902ef0ef7b69a4dd12a4b6c6084
Catherine Turner <cvturner@usc.edu>
2020-04-12 19:05:51 -0700

    Removed Location.java methods

--- src/models/Location.java
+++ src/models/Location.java
@@ -3,45 +3,25 @@ package models;
 import java.util.ArrayList;
 
 public class Location {
-	public int locationID;
-	public ArrayList<Integer> reviews;
+	int locationID;
+	String name;
+	String address;
+	ArrayList<Integer> reviews;
+	String phoneNumber;
+	String website;
 	
-	public Location(int locID) {
+	// Instantiate Location object
+	public Location(int locID,
+					String locName,
+					String locAddress,
+					ArrayList<Integer> locReviews,
+					String locPhone,
+					String locSite) {
 		this.locationID = locID;
-	}
-	
-	// Return the name of a location
-	public String getName(int locationID) {
-		String name = "";
-		name = SQLCalls.locationToName(locationID);
-		return name;
-	}
-	
-	// Return the address of a location
-	public String getAddress(int locationID) {
-		String address = "";
-		address = SQLCalls.locationToAddress(locationID);
-		return address;
-	}
-	
-	// Return the average ratings of a location in an array list
-	public ArrayList<Integer> getAverages(int locationID) {
-		ArrayList<Integer> averages = new ArrayList<Integer>();
-		averages = SQLCalls.locationToAverages(locationID);
-		return averages;
-	}
-	
-	// Return the phone number of a location as a String
-	public String getPhone(int locationID) {
-		String phoneNum = "";
-		phoneNum = SQLCalls.locationToPhone(locationID);
-		return phoneNum;
-	}
-	
-	// Return the website URL of a location
-	public String getURL(int locationID) {
-		String URL = "";
-		URL = SQLCalls.locationToURL(locationID);
-		return URL;
+		this.name = locName;
+		this.address = locAddress;
+		this.reviews = locReviews;
+		this.phoneNumber = locPhone;
+		this.website = locSite;
 	}
 }
