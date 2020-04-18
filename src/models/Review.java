6c49436b4ca4ea26a96d0f5feba66ca9d536df6f
Catherine Turner <cvturner@usc.edu>
2020-04-17 20:16:21 -0700

    Updated Reviews table to include ratings, changed SQLCalls to reflect

--- src/models/Review.java
+++ src/models/Review.java
@@ -3,14 +3,16 @@ package models;
 public class Review {
 	private String title;
 	private String body;
+	private double rating;
 	private String userName;
 	private int upvotes;
 	private int downvotes;
 	private Location location;
 	
-	public Review(String title, String body, String userName, int upvotes, int downvotes, Location location) {
+	public Review(String title, String body, double rating, String userName, int upvotes, int downvotes, Location location) {
 		this.title = title;
 		this.body = body;
+		this.rating = rating;
 		this.userName = userName;
 		this.upvotes = upvotes;
 		this.downvotes = downvotes;
