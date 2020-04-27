8981f425cb10a56337eea9b0f2b07ec1576ae567
Catherine Turner <cvturner@usc.edu>
2020-04-26 19:56:05 -0700

    Added some users and reviews

--- sql/DBDeply.sql
+++ sql/DBDeply.sql
@@ -69,5 +69,23 @@ INSERT INTO Locations (LocationName, Address, PhoneNumber, Website, Latitude, Lo
 ("Los Angeles Coliseum", "3911 Figueroa St, Los Angeles, CA 90037", "2137477111", "www.lacoliseum.com", 34.01402750912101, -118.28799243550748),
 ("Treehouse Rooftop", "686 N Spring St, Los Angeles, CA 90012", "2139887064", "treehouserooftop.com", 34.05952999885075, -118.23786257766187),
 ("Grace Ford Salvatori Hall", "900 W 36th St, Los Angeles, CA 90089", "2137402666", "https://calendar.usc.edu/grace_ford_salvatori_hall_gfs", 34.02127513124883, -118.28800316434354),
-("Everybody's Kitchen", "642 W 34th St, Los Angeles, CA 90007", "2137400269", "https://hospitality.usc.edu/dining_locations/everybodys-kitchen/", 34.02112610919359, -118.28215833753347),,
+("Everybody's Kitchen", "642 W 34th St, Los Angeles, CA 90007", "2137400269", "https://hospitality.usc.edu/dining_locations/everybodys-kitchen/", 34.02112610919359, -118.28215833753347),
 ("Trader Joe's", "3131 S Hoover St Ste 1920, Los Angeles, CA 90089", "2137491497", "https://locations.traderjoes.com/ca/los-angeles/250/", 34.02606569046466, -118.28483912162483);
+
+-- ---------------------- Add users ---------------------- 
+INSERT INTO Users (Email, Name, Username, Password, City, Stars, Verified, Handicap) VALUES
+("imangry@gmail.com", "John Doe", "johndoe23", "password", "Los Angeles", "1", "1", "None"),
+("president@hotmail.com", "Barack Obama", "bobama", "usa", "Washington D.C.", "5", "1", "None"),
+("happywheels@usc.edu", "Roberta Roller", "theyseemerollin", "12345", "Atlanta", "4", "0", "Wheelchair user");
+
+-- ---------------------- Add reviews ---------------------- 
+INSERT INTO Reviews (LocationID, UserID, Title, Body, ElevatorRating, RampRating, DoorRating,
+OtherRating, Upvotes, Downvotes) VALUES
+("2", "1", "This place is terrible.", "Not only is their food terrible, but there is no automatic door and they didn't open it for me for five minutes.", "1", "1", "1", "1", 0, 5),
+("2", "2", "A great Thai-me!", "Great service, happy to help, and very zen", "5", "5", "5", "5", 11, 1),
+("1", "1", "Chicken too hot", "The chicken is too hot and didn't help my asthma.", "1", "1", "1", "1", 0, 2),
+("1", "3", "So happy", "Dave's Hot Chicken left me so happy that I honestly didn't notice any issues with the place.", "5", "5", "5", "5", 2, 4),
+("8", "3", "Stairway to Hell", "This building has so many unnecessary stairs. You can't access most of the sitting areas if you're traveling in a wheelchair, which is really disappointing for a college campus.", "3", "1", "1", "1", 10, 2),
+("8", "1", "Disgusting", "Not only is GFS super dark and depressing, but the doors are usually locked and super heavy, so you have to wait for someone to run over and let you in. Made me late to a meeting.", "2", "1", "1", "2", 5, 0),
+("10", "2", "Give it a medal!", "I love this store! Automatic doors, enough space to move around, and free samples! What could be better?", "5", "5", "5", "5", 5, 1),
+("10", "1", "Satisfied", "Even I can admit that this is an accessible abode on a campus that doesn't seem to care much about our community. Still, they're lucky there's no rating for gentrification.", "5", "5", "5", "4", 11, 1);
